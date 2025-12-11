9ab3a0d160e0234ae4f560e9911d2f3a6b6d9e9b
EfraimGottlieb <efgo583208979@gmail.com>
2025-12-11 11:14:59 +0200

    added buyStock

--- utils/stockManagment.js
+++ utils/stockManagment.js
@@ -1,6 +1,15 @@
 import { stockMarket } from "../data/stocks.js";
 const stocks = stockMarket.stocks;
 
+const getCategoryIndexes = (category) => {
+  const indexes = [];
+  for (let i = 0; i < stocks.length - 1; i++) {
+    if (stocks[i].category === category) {
+      indexes.push(i);
+    }
+  }
+  return indexes;
+};
 const getIndexOfStock = (stockId) =>
   stocks.findIndex((stock) => stock.id == stockId);
 
@@ -8,25 +17,44 @@ const updateLastUpdated = () => {
   stockMarket.lastUpdated = new Date();
 };
 
-const addToPreviousPrices = (stockId, value) => {
-  stocks[stockId].previousPrices.push(value);
+const addToPreviousPrices = (stockIndex, value) => {
+  stocks[stockIndex].previousPrices.push(value);
 };
 
-const updateStockPrice = (stockId, increment) => {
-  stocks[stockId].currentPrice = stocks[stockId].currentPrice * increment;
+const updateStockPrice = (stockIndex, increment) => {
+  const newPrice = stocks[stockIndex].currentPrice * increment;
+  stocks[stockIndex].currentPrice = newPrice;
+};
+
+const updateCategoryPrice = (category, increment) => {
+  const categoryIndexes = getCategoryIndexes(category);
+  categoryIndexes.forEach((index)=>{
+    addToPreviousPrices(index, stocks[index].currentPrice)
+    updateStockPrice(index, increment)
+
+  })
 };
 
 const saleStock = (stockId) => {
+  const stockIndex = getIndexOfStock(stockId);
+  stocks[stockIndex].availableStocks --
   updateLastUpdated();
-  addToPreviousPrices(stockId, stocks[stockId].currentPrice);
-  updateStockPrice(stockId, 1.05);
-  stocks[stockId].availableStocks--;
+  addToPreviousPrices(stockIndex, stocks[stockIndex].currentPrice);
+  updateStockPrice(stockIndex, 1.05);
+  updateCategoryPrice(stocks[stockIndex].category, 1.01);
 };
-console.log(stocks[0]);
-console.log(stockMarket.lastUpdated);
-saleStock(getIndexOfStock("x7l2df9"));
-console.log(stockMarket.lastUpdated);
-console.log(stocks[0]);
+
+const buyStock = (stockId) => {
+  const stockIndex = getIndexOfStock(stockId);
+  stocks[stockIndex].availableStocks ++
+  updateLastUpdated();
+  addToPreviousPrices(stockIndex, stocks[stockIndex].currentPrice);
+  updateStockPrice(stockIndex, 0.95);
+  updateCategoryPrice(stocks[stockIndex].category, 0.99);
+};
+
+
+
 // const  searchStock = (identifier) => {}
 // const  filterStocksByPrice =(givenPrice, above) => {}
 // const  OperateOnStock = (operation, identifier) => {
